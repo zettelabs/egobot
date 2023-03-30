e91d7f75af11227726d7cd5d06e81db61d932590
Celil Şahin <cengcelil@gmail.com>
2023-03-30 15:43:31 +0300

    axios

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
     const { message } = JSON.parse(event.body);
     const { command, botName, extra } = messageParts(message.text);
 
-    if (botName === "partiibot" || botName === null) {
+    if (botName === "tgegobot" || botName === null) {
         switch (command) {
             case "echo":
                 await sendMessage(message.chat.id, extra || "ECHO!");
