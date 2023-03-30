ba06774c936df413b857a4b23232ad593822a37d
Celil Şahin <cengcelil@gmail.com>
2023-03-30 15:49:56 +0300

    axios

--- hashnode.js
+++ hashnode.js
@@ -1,21 +1,21 @@
 const axios = require("axios").default;
 
-const get = async (query) => {
-  const res = await axios.get("http://88.255.141.66/mblSrv14/service.asp?FNC=Otobusler&VER=3.1.0&LAN=tr&DURAK=10512", { query });
+const get = async() => {
+  const res = await axios.get("http://88.255.141.66/mblSrv14/service.asp?FNC=Otobusler&VER=3.1.0&LAN=tr&DURAK=10512");
   return res.data.data;
 };
 
-const featuredPosts = `
-  query {
-    storiesFeed(type: FEATURED) {
-      author {
-        username
-      }
-      title
-    }
-  }
-`;
+// const featuredPosts = `
+//   query {
+//     storiesFeed(type: FEATURED) {
+//       author {
+//         username
+//       }
+//       title
+//     }
+//   }
+// `;
 
 module.exports = {
-  getFeaturedPosts: () => get(featuredPosts),
+  getFeaturedPosts: () => get(),
 };
