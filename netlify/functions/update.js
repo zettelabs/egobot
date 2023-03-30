83a8193fb802bf6ee77675788f99943cc4d55f7e
Celil Şahin <cengcelil@gmail.com>
2023-03-30 15:39:22 +0300

    axios

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,13 +1,34 @@
-exports.handler = async (event) => {
-    console.log("Received an update from Telegram!", event.body);
-    return { statusCode: 200 };
-};
-const axios = require("axios").default;
-module.exports = async (chat_id, text) => {
-    await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-        chat_id,
-        text,
-    });
+const sendMessage = require("../../sendMessage");
+const messageParts = require("../../messageParts");
+const hashnode = require("../../hashnode");
 
-    return true;
+exports.handler = async (event) => {
+    const { message } = JSON.parse(event.body);
+    const { command, botName, extra } = messageParts(message.text);
+
+    if (botName === "partiibot" || botName === null) {
+        switch (command) {
+            case "echo":
+                await sendMessage(message.chat.id, extra || "ECHO!");
+                break;
+
+            case "hashnodefeatured":
+                const { storiesFeed } = await hashnode.getFeaturedPosts();
+
+                const reply = `
+${storiesFeed[0].title} by ${storiesFeed[0].author.username}
+${storiesFeed[1].title} by ${storiesFeed[1].author.username}
+${storiesFeed[2].title} by ${storiesFeed[2].author.username}
+https://hashnode.com/featured
+`;
+
+                await sendMessage(message.chat.id, reply);
+                break;
+
+            default:
+                await sendMessage(message.chat.id, "I don't understand that command.");
+        }
+    }
+
+    return { statusCode: 200 };
 };
