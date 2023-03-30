27695a25cbab3dc5752e8e2d7d10d744396ac6c1
Celil Şahin <cengcelil@gmail.com>
2023-03-30 16:34:32 +0300

    axios

--- hashnode.js
+++ hashnode.js
@@ -1,13 +1,11 @@
 const axios = require("axios").default;
 const config = {
-  headers:{
+  headers : {
     'User-Agent': 'EGO Genel Mudurlugu-EGO Cepte-3.1.0',
+    'Host': '88.255.141.66'
   }
 };
-const get = async() => {
-  const res = await axios.get("http://88.255.141.66/mblSrv14/service.asp?FNC=Otobusler&VER=3.1.0&LAN=tr&DURAK=10512",config);
-  return res;
-};
+const get = async() => axios.get("http://88.255.141.66/mblSrv14/service.asp?FNC=Otobusler&VER=3.1.0&LAN=tr&DURAK=10512", config).toString();
 
 // const featuredPosts = `
 //   query {
